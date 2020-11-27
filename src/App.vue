99f5fe27a2a29f7f6c33b32725068f65fc14a10a
keyLJet <keyLJet@hotmail.com>
2020-11-27 14:52:26 +0800

    feat:test2

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,15 @@
   <div>
     <h3>App</h3>
     <p>content for test...</p>
+    <p>content for secondtest...</p>
   </div>
 </template>
 
 <script>
 export default {
-  name:'App',
-}
+  name: "App",
+};
 </script>
 
 <style>
-
 </style>
