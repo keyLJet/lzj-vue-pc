409de472fadd658f195efe6d8f19aaaa23192ee5
keyLJet <keyLJet@hotmail.com>
2020-11-29 17:36:06 +0800

    拦截器和代理服务器配置

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>Login组件内容...</div>
+  <div>
+    Login组件内容...
+    <button @click="login">login</button>
+  </div>
 </template>
 
 <script>
+import { reqLogin } from "@api/user";
+
 export default {
   name: "Login",
+  methods: {
+    login() {
+      reqLogin('13700000000','111111')
+      .then((res) =>{
+        console.log('res',res)
+      })
+      .catch((err) =>{
+        console.log('err',err)
+      })
+    },
+  },
 };
 </script>
 
